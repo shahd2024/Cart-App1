83316bb014eaf6266f9a099d387bd04479947255
shahd2024 <shahdmahmoudosman@gmail.com>
2025-07-07 19:48:52 +0300

    commit 4

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,22 +21,31 @@ export function Navbar() {
     <>
    
 
-{/* <nav className="bg-gray-100 border-gray-200 fixed top-0 start-0 end-0 z-30 cursor-pointer px-3">
+<nav className="bg-gray-100 border-gray-200 fixed top-0 start-0 end-0 z-30 cursor-pointer px-3 text-center">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between   mx-auto py-2">
     <Link to="" className="flex items-center  space-x-3 rtl:space-x-reverse ">
      <img src={logo} className="h-8  " alt="Flowbite Logo" />
     </Link>
    
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200   " aria-controls="navbar-default" aria-expanded="false">
-      <span className="sr-only">Open main menu</span>
-      <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
-      </svg>
-    </button>
+
+    <button
+  onClick={() => setIsOpen(!isOpen)}
+  type="button"
+  className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+  aria-controls="navbar-default"
+  aria-expanded={isOpen}
+>
+  <span className="sr-only">Open main menu</span>
+  <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
+  </svg>
+</button>
  
 
   
-       <div className="hidden w-full md:w-auto md:block  lg:flex     " id="navbar-default">
+       
+       <div className={`${isOpen ? 'block' : 'hidden'} w-full md:w-auto md:block  lg:flex`} id="navbar-default">
+
     
        <ul className="sm:me-0 md:me-0 lg:me-[100px]  font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-100  ">
        {userLogin?<>
@@ -105,8 +114,8 @@ export function Navbar() {
   
    </div>
   </div>
-</nav> */}
- <nav className="bg-gray-100 border-gray-200 fixed top-0 start-0 end-0 z-30 px-3 text-center">
+</nav>
+ {/* <nav className="bg-gray-100 border-gray-200 fixed top-0 start-0 end-0 z-30 px-3 text-center">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2">
         <Link to="" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} className="h-8" alt="Logo" />
@@ -169,7 +178,7 @@ export function Navbar() {
           </button>
         </div>
       </div>
-    </nav>
+    </nav> */}
 
 
     </>
